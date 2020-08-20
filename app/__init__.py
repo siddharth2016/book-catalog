46a43095377ae90b2699e1e34cd85397ae00fb01
Siddharth2016 <siddharthchandragzb@gmail.com>
2020-08-20 18:17:16 +0530

    create_app factory and add catalog blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,21 @@
+# app/__init__.py
+
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+def create_app(config_type):
+
+    flask_app = Flask(__name__)
+
+    configuration = os.path.join(os.getcwd(), 'config', config_type + '.py')
+    flask_app.config.from_pyfile(configuration)
+
+    db.init_app(flask_app)
+
+    from app.catalog import main
+
+    flask_app.register_blueprint(main)
